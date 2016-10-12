20771ad5d6c193460eb9eb0890fb1366d6cb98db
Damien Salvador <vador@grabeuh.com>
2016-10-12 12:24:29 +0200

    Reformat code

---
+++ src/test/java/OraclePaddingClientTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dame on 12/10/2016.
+ */
+public class OraclePaddingClientTest {
+    @Test
+    public void stringAsHexFromBytes() throws Exception {
+
+    }
+
+    @Test
+    public void bytesFromStringAsHex() throws Exception {
+
+    }
+
+}
